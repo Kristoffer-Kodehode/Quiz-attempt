3a977bff39c457715b6c45999c68cf4d4e7d91f0
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-12-21 13:53:19 +0100

    changed answers to numbers

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ questions = {
     Q1: {
         Q: "What day was JavaScript released?",
         O: ["May 23rd, 1995", "Nov 24th, 1995", "Dec 4th, 1995", "Dec 17th, 1996"],  
-        A: "Dec 4th, 1995"
+        A: 3 #"Dec 4th, 1995"
     }, 
     Q2: {
         Q: "Inside which HTML element do we put the JavaScript?",
         O: ["<script>", "<scripting>", "<js>", "<javascript>"],  
-        A: "<script>"
+        A: 1 #"<script>"
     }, 
     Q3: {
         Q: "What is the correct JavaScript syntax to change the content of the HTML element below? <p id='demo'> This is a demonstration.</p>?",
@@ -17,7 +17,7 @@ questions = {
             "document.getElement('p').innerHTML = 'Hello World!';", 
             "#demo.innerHTML = 'Hello World!';"
         ],  
-        A: "document.getElementById('demo').innerHTML = 'Hello World!';"
+        A: 2 #"document.getElementById('demo').innerHTML = 'Hello World!';"
     }, 
     Q4: {
         Q: "Where is the correct place to insert a JavaScript?",
@@ -25,19 +25,19 @@ questions = {
             "The <body> section",
             "The <head> section",
             "Both the <head> section and the <body> section are correct"],  
-        A: "Both the <head> section and the <body> section are correct"
+        A: 3 #"Both the <head> section and the <body> section are correct"
     }, 
     Q5: {
         Q: "What is the correct syntax for referring to an external script called 'xxx.js'?",
         O: ["<script name='xxx.js'>', '<script src='xxx.js'>', '<script href='xxx.js'>"],  
-        A: "<script src='xxx.js'>"
+        A: 2 #"<script src='xxx.js'>"
     }, 
 }
 
 puts "Welcome to this Javascript quiz that i made in Ruby and stole all the questions for! (I previously made one in node and most of those questions were taken from W3 schools' JS quiz)"
 
 puts "How to play:\n 
-Read each question and your options properly, then write your answer exactly as it is written in the options."
+Read each question and your options properly, then enter the number of the option you think is correct."
 
 Q1 = questions[:Q1]
 Q2 = questions[:Q2]
@@ -45,8 +45,7 @@ Q3 = questions[:Q3]
 Q4 = questions[:Q4]
 Q5 = questions[:Q5]
 
-def showOptions(o) o.each { |x| puts x } #.with_index(1) { |x, index| puts "#{index}. #{x}"}
-end
+
 
 class Quiz
 
@@ -59,6 +58,12 @@ class Quiz
   @@score = 0
   @@count = 0
 
+  def showOptions(o) o.each { |x| puts x }
+  end
+  
+  def showOptionsI(o) o.each.with_index(1) { |x, index| puts "#{index}. #{x}"}
+  end
+
   def incScore 
     return @@score += 1
   end
@@ -69,10 +74,10 @@ class Quiz
   
   def ask
     puts "\n#{@question}\n\n"
-    showOptions(@options)
+    showOptionsI(@options)
     puts "\nYour answer:\n"
-    answer = gets.chomp
-    unless answer != @answer
+    answer = gets
+    unless answer.to_i != @answer.to_i
         puts "Correct!\n" 
         incScore
     else
